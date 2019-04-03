8bc9e7164548b941a2aed04d83ef0ca5ac0dddc6
Leo Tran <minhdangcntt@gmail.com>
2019-04-03 18:21:40 +0700

    add jwt

---
+++ jwt/middleware.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const config  = require('./config');
+module.exports = (req, res, next) => {
+    // get header, express header auto convert to lowercase
+    let token = req.headers['x-access-token'] || req.headers['authorization'];
+    if (token.startsWith('Bearer ')) {
+        // Remove Bearer from string
+        token = token.slice(7, token.length);
+    }
+    if(token){
+        jwt.verify(token, config.secret, (err, decoded) => {
+            if(err){
+                return res.status(200).json({
+                    message: 'Token is not valid'
+                });
+            }
+            req.decoded = decoded;
+            next();
+        })
+    } else {
+        res.status(403).json({
+            message:'Token not exist'
+        });
+    }
+}
